ab61f8fda276ac4a4b65577b11ad25c1d6829216
devesh tomar <tomardevesh012@gmail.com>
2026-07-09 02:50:34 +0530

    Done Dashboard Backend

---
+++ module/github/lib/github.ts
@@ -0,0 +1,70 @@
+import { Octokit } from "octokit";
+import { auth } from "../../../lib/auth";
+import { prisma } from "../../../lib/db";
+import { headers } from "next/headers";
+export { headers } from "next/headers";
+
+//For getting github access token from the request headers
+export const getGithubToken = async () => {
+  const session = await auth.api.getSession({ headers: await headers() });
+  if (!session) {
+    throw new Error("Unauthorized");
+  }
+  const account = await prisma.account.findFirst({
+    where: {
+      userId: session.user.id,
+      providerId: "github",
+    },
+  });
+  if (!account) {
+    throw new Error("No access token found for the user");
+  }
+  return account.accessToken;
+};
+
+// lets now fetch the github user data using the access token
+export async function fetchUserContribution(token: string | null, username: string) {
+  const octokit = new Octokit({
+    auth: token,
+  });
+  const query = `
+  query ($username: String!) {
+    user(login: $username) {
+      contributionsCollection {
+        contributionCalendar {
+          totalContributions
+          weeks {
+            contributionDays {
+              date
+              contributionCount
+              colour}
+              }
+            }
+        }
+      }
+          }`;
+
+          interface Contributiondata {
+           user:{
+            contributionsCollection:{
+              contributionCalendar:{
+                totalContributions: number;
+                weeks: {
+                    contributionCount: number;
+                    date: string;
+                    colour: string;
+                }[];
+           }
+          }
+        }
+    }
+          try{
+const response: Contributiondata = await octokit.graphql(query, {
+  username: username,
+});
+return response.user.contributionsCollection.contributionCalendar;
+          }
+          catch (error) {
+ throw new Error("Error fetching user contribution data: " + error);
+          }
+}
