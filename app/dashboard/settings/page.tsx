b62a6580abd23ff927618b0d1258e9a2de453705
devesh tomar <tomardevesh012@gmail.com>
2026-07-12 16:39:58 +0530

    Done Repo Fetching and webhook

---
+++ app/dashboard/settings/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React from 'react'
+
+const SettingPage = () => {
+  return (
+    <div className='space-y-6'>
+        <div className=''></div>
+      
+    </div>
+  )
+}
+
+export default SettingPage
